0771f5206bc02856f1919fcf77df40005e5b1937
Thomas Guillory <thomasvguillory@gmail.com>
2026-04-03 20:34:49 +0200

    stash

--- src/main.rs
+++ src/main.rs
@@ -9,62 +9,71 @@ pub use filter::FilterType;
 mod buffer;
 pub use buffer::DelayLine;
 
-fn shaper(in_value: f32, min: f32, max: f32, curve: f32)->f32{
+fn shaper(in_value: f32, min: f32, max: f32, curve: f32) -> f32 {
     //normalize to [0 ; 1]
-    let mut value = (in_value-min)/(max-min);
-    value = ((curve*value).exp()-1.0)/((curve).exp()-1.0);
-    value = (value * max)+min;
+    let mut value = (in_value - min) / (max - min);
+    value = ((curve * value).exp() - 1.0) / ((curve).exp() - 1.0);
+    value = (value * max) + min;
     value
 }
 
-struct Random{
-    seed:f32,
-    min:f32,
-    max:f32,
-    shaped_value:f32,
-    m:f32,
-    smoothing:f32,
+struct Random {
+    seed: f32,
+    min: f32,
+    max: f32,
+    shaped_value: f32,
+    m: f32,
+    smoothing: f32,
 }
 
-impl Random{
+impl Random {
+    pub fn new() -> Random {
+        Random {
+            seed: 123456789.0,
+            min: 0.0,
+            max: 1.0,
+            shaped_value: 0.0,
+            m: 126379272.0,
+            smoothing: 0.0,
+        }
+    }
 
-pub fn new()->Random
-{
-    Random { seed: 123456789.0, min: 0.0, max: 1.0 , shaped_value: 0.0, m: 126379272.0, smoothing: 0.0}
+    pub fn new_min_max(min: f32, max: f32, smoothing: f32) -> Random {
+        Random {
+            seed: 123456789.0,
+            min,
+            max,
+            shaped_value: 0.0,
+            m: 126379272.0,
+            smoothing,
+        }
+    }
+
+    pub fn process(&mut self) -> f32 {
+        self.seed = (1103515245.0 * self.seed + 5.0) % self.m;
+        let prev_value = self.shaped_value;
+        self.shape();
+        self.shaped_value =
+            (self.shaped_value * (1.0 - self.smoothing)) + (prev_value * (self.smoothing));
+        return self.shaped_value;
+    }
+
+    fn shape(&mut self) {
+        self.shaped_value = self.seed / self.m;
+        self.shaped_value = self.shaped_value * self.max;
+        self.shaped_value = self.shaped_value + self.min;
+    }
 }
 
-pub fn new_min_max(min:f32, max:f32, smoothing:f32)->Random
-{
-    Random { seed: 123456789.0, min, max, shaped_value: 0.0, m: 126379272.0, smoothing}
-}
-
-pub fn process(&mut self)->f32{
-    self.seed = (1103515245.0 * self.seed + 5.0) % self.m;
-    let prev_value = self.shaped_value;
-    self.shape();
-    self.shaped_value = (self.shaped_value * (1.0-self.smoothing)) + (prev_value * (self.smoothing));
-    return self.shaped_value;
-}
-
-fn shape(&mut self){
-    self.shaped_value = self.seed / self.m;
-    self.shaped_value = self.shaped_value * self.max;
-    self.shaped_value = self.shaped_value + self.min;
-}
-
-}
-
-
-fn rand(seed:f32)->f32
-{
-  let seed = (1103515245.0 * seed + 5.0) % 126379272.0;
-  return seed;
+fn rand(seed: f32) -> f32 {
+    let seed = (1103515245.0 * seed + 5.0) % 126379272.0;
+    return seed;
 }
 
 fn main() -> anyhow::Result<()> {
     // Collect all arguments
     let args: Vec<String> = env::args().collect();
-    
+
     // Path of input file is the first argument
     let in_path = &args[1];
     let out_path = &args[2];
@@ -79,7 +88,6 @@ fn main() -> anyhow::Result<()> {
 
     let dynimg = image::open(in_path)?;
 
-    
     let mut bufr = DelayLine::new(1000.0, buffer::DelayMode::Comb);
     let _bufg = DelayLine::new(1000.0, buffer::DelayMode::Comb);
     let _bufb = DelayLine::new(1000.0, buffer::DelayMode::Comb);
@@ -89,13 +97,12 @@ fn main() -> anyhow::Result<()> {
     let mut filter_r = Biquad::new(FilterType::LPF);
     let mut filter_g = Biquad::new(FilterType::PEAK);
     let filter_b = Biquad::new(FilterType::LPF);
-    
+
     filter_r.init(44000.0);
-    filter_r.set_frequence_and_resonance(3000.0, 7.0);
-    
+    filter_r.set_frequence_and_resonance(20000.0, 0.9);
+
     filter_g.init(44000.0);
     filter_g.set_frequence_and_resonance(2000.0, 100.0);
-    
 
     // bufr.set_feedback(0.0);
 
@@ -113,13 +120,13 @@ fn main() -> anyhow::Result<()> {
     let _signal = vec![0.0, 0.0];
     let mut columns: Vec<Vec<f32>> = vec![vec![0.0, 0.0]; width as usize];
 
-    let mut random_generator = Random::new_min_max(0.0, 1.0, 0.99);
+    let mut random_generator = Random::new_min_max(0.0, 100.0, 0.99);
 
     for pixel in bufimg.enumerate_pixels_mut() {
         //update the buffer for the dematricing
         let color = bayer_matrix[(pixel.0 % 2) as usize][(pixel.1 % 2) as usize];
         // apply filter to the new pixel and push it on the buffer
-        columns[pixel.0 as usize].push(pixel.2[color] as f32);
+        columns[pixel.0 as usize].push(pixel.2[color] as f32 + random_generator.process());
         // signal.push(pixel.2[color] as f32);
     }
 
@@ -128,54 +135,19 @@ fn main() -> anyhow::Result<()> {
     // }
 
     for column in columns.iter_mut() {
-        let mut prev_sample = 0.0;
-        let local_feedback = 0.99 + random_generator.process()/100.0;
-        
-        for sample in column.iter_mut() {
-            let mut temp = prev_sample * (local_feedback) + *sample * (1.0-local_feedback);
-            // if prev_sample < 50.0 {
-            //     temp = *sample;
-            // }
 
-            *sample = filter_r.process(*sample);
-            // *sample = filter_g.process(*sample);
-            prev_sample = temp; // shaper(temp, 0.0, 255.0, 0.001);
+        for (idx, sample) in column.iter_mut().enumerate() {
+            if idx % 3== 1 {
+                *sample = *sample - 200.0;
+            } else {
+                *sample = *sample + 200.0;
+            }
         }
 
-        for idx in 0..100{
-            filter_r.process(0.0);
-        }
-        for sample in column.iter_mut().rev() {
-            let mut temp = prev_sample * (local_feedback) + *sample * (1.0-local_feedback);
-            // if prev_sample < 50.0 {
-            //     temp = *sample;
-            // }
-
-            *sample = filter_r.process(*sample);
-            // *sample = filter_g.process(*sample);
-            prev_sample = temp; // shaper(temp, 0.0, 255.0, 0.001);
-        }
-
-        for idx in 0..100{
-            filter_r.process(0.0);
-        }
-
-        // douple pass in reverse
-        // for sample in column.iter_mut().rev() {
-        //     let mut temp = prev_sample * feedback + *sample * (1.0-feedback);
-        //     if prev_sample < 50.0 {
-        //         temp = *sample;
-        //     }
-        //     *sample = temp;
-        //     prev_sample = temp;
-        // }
-        for _idx in 0..1000 {
-            bufr.process(0.0);
-        }
     }
 
     for pixel in bufimg.enumerate_pixels_mut() {
-        let color = bayer_matrix[((pixel.0+1) % 2) as usize][((pixel.1+1) % 2) as usize];
+        let color = bayer_matrix[((pixel.0 + 1) % 2) as usize][((pixel.1 + 1) % 2) as usize];
 
         // Bayer reconstruction
         let mut r = 0;
